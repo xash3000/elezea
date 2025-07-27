53fc362438c706d2830874124f60a28cedc9f77e
Ali <alifaki077@gmail.com>
2025-07-27 14:35:17 +0300

    fix: apply migration after column rename

---
+++ backend/Elezea/Migrations/20250727113442_RenameDescriptionColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Elezea.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameDescriptionColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Submissions",
+                newName: "UserDescription");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserDescription",
+                table: "Submissions",
+                newName: "Description");
+        }
+    }
+}
